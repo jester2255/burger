c504cc2d95e5a37e19956f000a64df03824ff605
jester2255 <jester2255pdunn@yahoo.com>
2017-10-02 08:38:08 -0600

    Something

--- config/orm.js
+++ config/orm.js
@@ -28,19 +28,18 @@ function objToSql(ob) {
     var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-      //if (typeof value === "string" && value.indexOf(" ") >= 0) {
-       // value = "'" + value + "'";
+      //if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+       value = "'" + value + "'";
       }
       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
       // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
-  
+  }
    // translate array of strings to a single comma-separated string
   return arr.toString();
 }
-
 var orm ={
   selectAll: function(tableInput, cb){
     var queryString = "SELECT * FROM ??";
@@ -83,9 +82,9 @@ var orm ={
 
 	    console.log(queryString);
 	    connection.query(queryString, function(err, result) {
-      if (err) {
+      /*if (err) {
         throw err;
-      }
+      }*/
       cb(result);
     });
   }
