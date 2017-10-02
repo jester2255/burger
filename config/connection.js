3ebc54a54786425189cc93fe5c68f019a326b5e8
jester2255 <jester2255pdunn@yahoo.com>
2017-10-02 07:18:24 -0600

    trying again

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 //mysql connection set up 
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+
+
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,7 +13,7 @@ var connection = mysql.createConnection({
   password: "password",//pasword for desktop || 5522retsej for laptop
   database: "burgers_db"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-const { Client } = require('pg');
 
 var port = process.env.PORT || 3000;
 
 var app = express();
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
